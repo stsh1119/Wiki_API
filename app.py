4f67a5831c6a049adcb04c45ac91088a214d3ce9
stsh1119 <stshlaptop@gmail.com>
2020-12-23 21:31:23 +0200

    [+] config changes

--- app.py
+++ app.py
@@ -4,27 +4,23 @@ import db
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
     results = []
     for row in db.view_all_articles():
-        result = {'id': row[0],
-                  'title': row[1],
-                  'text': row[2],
-                  'is_current': row[3]
-                  }
+        result = {"id": row[0], "title": row[1], "text": row[2], "is_current": row[3]}
         results.append(result)
     return jsonify(results)
 
 
-@app.route('/add_page', methods=['POST'])
+@app.route("/add_page", methods=["POST"])
 def add_page():
     if request.is_json:
         title = request.json.get("title")
         body = request.json.get("body")
         # return request.get_json()
-        return f'''{title} --- {body}'''
+        return f"""{title} --- {body}"""
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
